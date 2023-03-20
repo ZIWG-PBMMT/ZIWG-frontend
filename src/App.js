5b6c3438be27f9855d1dfee83391c13c785c50ee
krasickiPawel <252749@student.pwr.edu.pl>
2023-03-20 21:48:24 +0100

    pierwszy działający podgląd kamery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
+// import Webcam from 'react-webcam';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Siema, jestem Paweł!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // WebcamComponent
+    <h1> Wojownicy Wyspy ! </h1>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Webcam from 'react-webcam'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const WebcamComponent = () => <Webcam />
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Webcam />
     <App />
   </React.StrictMode>
 );
