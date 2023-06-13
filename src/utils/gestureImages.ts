0ec8c93effa82c323f9da985b827acfb899d7e6a
krasickiPawel <252749@student.pwr.edu.pl>
2023-06-13 03:28:09 +0200

    front prepared to use only 5 selected signs

--- src/utils/gestureImages.ts
+++ src/utils/gestureImages.ts
@@ -4,10 +4,10 @@ export type GestureImage = {
 };
 
 const gestureImages: GestureImage[] = [
-  {
-    name: "a.jpg",
-    expectedGesture: "A",
-  },
+  // {
+  //   name: "a.jpg",
+  //   expectedGesture: "A",
+  // },
   {
     name: "b.jpg",
     expectedGesture: "B",
@@ -16,98 +16,98 @@ const gestureImages: GestureImage[] = [
     name: "c.jpg",
     expectedGesture: "C",
   },
-  {
-    name: "d.jpg",
-    expectedGesture: "D",
-  },
+  // {
+  //   name: "d.jpg",
+  //   expectedGesture: "D",
+  // },
   {
     name: "e.jpg",
     expectedGesture: "E",
   },
-  {
-    name: "f.jpg",
-    expectedGesture: "F",
-  },
+  // {
+  //   name: "f.jpg",
+  //   expectedGesture: "F",
+  // },
   {
     name: "g.jpg",
     expectedGesture: "G",
   },
-  {
-    name: "h.jpg",
-    expectedGesture: "H",
-  },
-  {
-    name: "i.jpg",
-    expectedGesture: "I",
-  },
-  {
-    name: "j.jpg",
-    expectedGesture: "J",
-  },
-  {
-    name: "k.jpg",
-    expectedGesture: "K",
-  },
-  {
-    name: "l.jpg",
-    expectedGesture: "L",
-  },
+  // {
+  //   name: "h.jpg",
+  //   expectedGesture: "H",
+  // },
+  // {
+  //   name: "i.jpg",
+  //   expectedGesture: "I",
+  // },
+  // {
+  //   name: "j.jpg",
+  //   expectedGesture: "J",
+  // },
+  // {
+  //   name: "k.jpg",
+  //   expectedGesture: "K",
+  // },
+  // {
+  //   name: "l.jpg",
+  //   expectedGesture: "L",
+  // },
   {
     name: "m.jpg",
     expectedGesture: "M",
   },
-  {
-    name: "n.jpg",
-    expectedGesture: "N",
-  },
-  {
-    name: "o.jpg",
-    expectedGesture: "O",
-  },
-  {
-    name: "p.jpg",
-    expectedGesture: "P",
-  },
-  {
-    name: "q.jpg",
-    expectedGesture: "Q",
-  },
-  {
-    name: "r.jpg",
-    expectedGesture: "R",
-  },
-  {
-    name: "s.jpg",
-    expectedGesture: "S",
-  },
-  {
-    name: "t.jpg",
-    expectedGesture: "T",
-  },
-  {
-    name: "u.jpg",
-    expectedGesture: "U",
-  },
-  {
-    name: "v.jpg",
-    expectedGesture: "V",
-  },
-  {
-    name: "w.jpg",
-    expectedGesture: "W",
-  },
-  {
-    name: "x.jpg",
-    expectedGesture: "X",
-  },
-  {
-    name: "y.jpg",
-    expectedGesture: "Y",
-  },
-  {
-    name: "z.jpg",
-    expectedGesture: "Z",
-  },
+  // {
+  //   name: "n.jpg",
+  //   expectedGesture: "N",
+  // },
+  // {
+  //   name: "o.jpg",
+  //   expectedGesture: "O",
+  // },
+  // {
+  //   name: "p.jpg",
+  //   expectedGesture: "P",
+  // },
+  // {
+  //   name: "q.jpg",
+  //   expectedGesture: "Q",
+  // },
+  // {
+  //   name: "r.jpg",
+  //   expectedGesture: "R",
+  // },
+  // {
+  //   name: "s.jpg",
+  //   expectedGesture: "S",
+  // },
+  // {
+  //   name: "t.jpg",
+  //   expectedGesture: "T",
+  // },
+  // {
+  //   name: "u.jpg",
+  //   expectedGesture: "U",
+  // },
+  // {
+  //   name: "v.jpg",
+  //   expectedGesture: "V",
+  // },
+  // {
+  //   name: "w.jpg",
+  //   expectedGesture: "W",
+  // },
+  // {
+  //   name: "x.jpg",
+  //   expectedGesture: "X",
+  // },
+  // {
+  //   name: "y.jpg",
+  //   expectedGesture: "Y",
+  // },
+  // {
+  //   name: "z.jpg",
+  //   expectedGesture: "Z",
+  // },
 ];
 
 export default gestureImages;
